21a3b66d4c853fee5fb2f5a48a27d5e780f03fee
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-08 01:18:30 -0300

    improved: logic used in event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,63 +11,25 @@ class EventController extends AbstractController
 {
     public function index(Request $request): Response
     {
-        return match ($request->json('type')) {
-            'deposit' => $this->deposity(
-                $request->json('destination'),
-                $request->json('amount'),
-            ),
-            'withdraw' => $this->withdraw(
-                $request->json('origin'),
-                $request->json('amount'),
-             ),
-            'transfer' => $this->transfer(
-                $request->json('origin'),
-                $request->json('destination'),
-                $request->json('amount'),
-             ),
-             default => \response('0', Response::HTTP_BAD_REQUEST),
-        };
-    }
+        $result = [];
+        $amount = $request->json('amount');
 
-    protected function deposity(string|int $account, int $income): Response
-    {
-        return response(['destination' => [
-            'id' => $account,
-            'balance' => $this->bankDataManagerService->incrementBalance($account, $income),
-        ]], Response::HTTP_CREATED);
-    }
-
-    protected function withdraw(string|int $account, int $outcome): Response
-    {
-        return response(['origin' => [
-            'id' => $account,
-            'balance' => $this->bankDataManagerService->decrementBalance($account, $outcome),
-        ]], Response::HTTP_CREATED);
-    }
-    
-    protected function transfer($origin, $destination, $amount): Response
-    {
-        return response([
-            'origin' => [
-                'id' => $origin,
-                'balance' => $this->bankDataManagerService->decrementBalance($origin, $amount),
-            ],
-            'destination' => [
-                'id' => $destination,
-                'balance' => $this->bankDataManagerService->incrementBalance($destination, $amount),
-            ],
-        ], Response::HTTP_CREATED);
-    }
-
-    /**
-     * @deprecated
-     */
-    protected function validateAccount($account): int
-    {
-        if (false !== $this->bankDataManagerService->accountExists($account)) {
-            return Response::HTTP_OK;
+        if ($origin = $request->json('origin')) {
+            $result = [
+                'origin' => [
+                    'id' => $origin,
+                    'balance' => $this->bankDataManagerService->decrementBalance($origin, $amount),
+                ],
+            ];
         }
 
-        return Response::HTTP_NOT_FOUND;
+        if ($destination = $request->json('destination')) {
+            $result['destination'] = [
+                'id' => $destination,
+                'balance' => $this->bankDataManagerService->incrementBalance($destination, $amount),
+            ];
+        }
+
+        return \response($result, Response::HTTP_CREATED);
     }
 }
