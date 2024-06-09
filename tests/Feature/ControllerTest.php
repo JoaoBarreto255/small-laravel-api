8d4b4e0491bc15501ae9722500f939df87f09b00
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-08 23:27:13 -0300

    test: base case for entire api

--- tests/Feature/ControllerTest.php
+++ tests/Feature/ControllerTest.php
@@ -7,12 +7,87 @@ use Tests\TestCase;
 class ControllerTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Run test on entire api.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function testBasicEntireApi(): void
     {
-        $response = $this->get('/');
+        // verify root uri.
+        $this->get('/')
+            ->assertNoContent();
 
-        $response->assertStatus(204);
+        // reset storage.
+        $this->post('/reset')
+            ->assertOk()
+            ->assertContent('OK');
+
+        // Seek for non-existent account
+        $this->get('/balance?account_id=1234')
+            ->assertNotFound()
+            ->assertContent('0');
+
+        // Create account
+        $this->postJson('/event', [
+            "type" => "deposit",
+            "destination" => "100",
+            "amount" => 10,
+        ])
+        ->assertCreated()
+        ->assertJson([
+            "destination" => ["id" =>"100", "balance" =>10]
+        ]);
+
+        // deposit into existent account.
+        $this->postJson('/event', [
+            "type" => "deposit",
+            "destination" => "100",
+            "amount" => 10,
+        ])
+        ->assertCreated()
+        ->assertJson([
+            "destination" => ["id" =>"100", "balance" => 20]
+        ]);
+
+        // Seek for non-existent account
+        $this->get('/balance?account_id=100')
+            ->assertOk()
+            ->assertContent('20');
+
+        // withdraw from existent account.
+        $this->postJson('/event', [
+            "type" => "withdraw",
+            "origin" => "100",
+            "amount" => 5,
+        ])
+        ->assertCreated()
+        ->assertJson([
+            "origin" => ["id" =>"100", "balance" => 15]
+        ]);
+
+        // withdraw from non-existent account.
+        $this->postJson('/event', [
+            "type" => "withdraw",
+            "origin" => "1234",
+            "amount" => 5,
+        ])
+        ->assertNotFound()
+        ->assertContent('0');
+
+        // transfer from one account into another.
+        $this->postJson('/event', [
+            "type" => "transfer",
+            "origin" => "100",
+            "destination" => "300",
+            "amount" => 15,
+        ])
+        ->assertCreated()
+        ->assertJson([
+            "origin" => ["id" =>"100", "balance" => 0],
+            "destination" => ["id" =>"300", "balance" => 15],
+        ]);
+
+        // reset storage.
+        $this->post('/reset')
+            ->assertOk()
+            ->assertContent('OK');
     }
 }
