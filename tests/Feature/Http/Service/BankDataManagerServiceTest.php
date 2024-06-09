19e1de804dbd430ae11b897ac05adae03f023088
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-08 22:07:00 -0300

    test: finished Service test

--- tests/Feature/Http/Service/BankDataManagerServiceTest.php
+++ tests/Feature/Http/Service/BankDataManagerServiceTest.php
@@ -22,7 +22,7 @@ class BankDataManagerServiceTest extends TestCase
         $this->cache = Cache::store('array');
     }
     
-    #[TestDox('Test BankDataManagerService getBalance')]
+    #[TestDox('Test getBalance')]
     public function testGetBalance()
     {
         $this->cache->put('100', 50);
@@ -32,5 +32,86 @@ class BankDataManagerServiceTest extends TestCase
         $this->assertSame(50, $service->getBalance(100));
         $this->assertFalse($service->getBalance('101'));
         $this->assertFalse($service->getBalance(101));
+    }
+
+    #[TestDox('Test setBalance')]
+    public function testSetBalance()
+    {
+        $service = new BankDataManagerService($this->cache);
+        $service->setBalance('100', 50);
+        $service->setBalance(10, 55);
+
+        $this->assertSame(50, $this->cache->get('100'));
+        $this->assertSame(55, $this->cache->get('10'));
+    }
+
+    #[TestDox('Test check if account exists')]
+    public function testAccountExists()
+    {
+        $this->cache->put('100', 50);
+        $service = new BankDataManagerService($this->cache);
+
+        $this->assertTrue($service->accountExists('100'));
+        $this->assertTrue($service->accountExists(100));
+        $this->assertFalse($service->accountExists('10'));
+        $this->assertFalse($service->accountExists(10));
+    }
+
+    #[TestDox('Test clear existent accounts')]
+    public function testClearBalances()
+    {
+        $this->cache->put('100', 50);
+        $service = new BankDataManagerService($this->cache);
+
+        $this->assertSame(50, $this->cache->get('100'));
+        $service->clearBalances();
+
+        $this->assertNull($this->cache->get('100'));
+    }
+
+    #[TestDox('Test increment one account')]
+    public function testIncrementBalance()
+    {
+        $this->cache->put('100', 50);
+        $service = new BankDataManagerService($this->cache);
+
+        $this->assertSame(50, $this->cache->get('100'));
+        $service->incrementBalance(100, 25);
+        $this->assertSame(75, $this->cache->get('100'));
+
+        $this->assertNull($this->cache->get('10'));
+        $service->incrementBalance(10, 25);
+        $this->assertSame(25, $this->cache->get('10'));
+
+        $this->assertSame(75, $this->cache->get('100'));
+        $service->incrementBalance(100, -25);
+        $this->assertSame(50, $this->cache->get('100'));
+
+        $this->assertNull($this->cache->get('11'));
+        $service->incrementBalance(11, -25);
+        $this->assertSame(-25, $this->cache->get('11'));
+    }
+
+#[TestDox('Test decrement one account')]
+    public function testDecrementBalance()
+    {
+        $this->cache->put('100', 50);
+        $service = new BankDataManagerService($this->cache);
+
+        $this->assertSame(50, $this->cache->get('100'));
+        $service->decrementBalance(100, 25);
+        $this->assertSame(25, $this->cache->get('100'));
+
+        $this->assertNull($this->cache->get('10'));
+        $service->decrementBalance(10, 25);
+        $this->assertSame(-25, $this->cache->get('10'));
+
+        $this->assertSame(25, $this->cache->get('100'));
+        $service->decrementBalance(100, -25);
+        $this->assertSame(50, $this->cache->get('100'));
+
+        $this->assertNull($this->cache->get('11'));
+        $service->decrementBalance(11, -25);
+        $this->assertSame(25, $this->cache->get('11'));
     }
 }
